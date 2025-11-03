9455479acafc98e69e2f236641729d12d66db960
Eugene <arhar18rus@gmail.com>
2025-11-03 01:37:29 -0800

    Respawn

--- globals/event_bus.gd
+++ globals/event_bus.gd
@@ -6,3 +6,5 @@ signal create_echo(echo_stats:EchoStats,position:Vector2)
 signal player_died()
 
 signal  reset_level()
+
+signal new_respawn(new_pos:Vector2)
--- systems/reset_system/reset_controller.gd
+++ systems/reset_system/reset_controller.gd
@@ -6,6 +6,7 @@ var player_respawn_position: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	EventBus.player_died.connect(_on_died)
+	EventBus.new_respawn.connect(_on_new_respawn)
 	_restart_timer= Timer.new()
 	_restart_timer.name ="RestartTimer"
 	_restart_timer.one_shot = true
@@ -27,3 +28,6 @@ func _on_restart_timer_timeout():
 	player_instance.add_to_group(&"Player")
 	owner.add_child(player_instance)
 	player_instance.global_position = player_respawn_position
+
+func _on_new_respawn(new_p:Vector2):
+	player_respawn_position = new_p
