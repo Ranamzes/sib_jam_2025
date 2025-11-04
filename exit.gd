58b2b69b45b408f3a19317270e7e03fe7e165f07
Dmitry Valov <d_valov@mail.ru>
2025-11-04 20:00:50 +0600

    Добавил переход на мини игру в конце уровня

--- exit.gd
+++ exit.gd
@@ -4,7 +4,8 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		var win_screen_scene = preload("res://world.tscn")
+		await get_tree().create_timer(1.5).timeout
+		var win_screen_scene = preload("res://puzzle/new_puzzle.tscn")
 		if win_screen_scene:
 			get_tree().change_scene_to_packed(win_screen_scene)	
 		else:
