15fc93a4a1653ea849a46ce3c19f8d862f42c2f7
Ranamzes <ranamzes@yandex.ru>
2026-02-15 17:22:46 +0600

    refactor: Migrate footstep_system to submodule

--- systems/footstep_system/footstep_audio_data.gd
+++
@@ -1,5 +0,0 @@
-class_name FootstepAudioData
-extends Resource
-
-## Словарь, где ключ - это имя поверхности (String), а значение - профиль звуков (SurfaceAudioProfile).
-@export var surface_profiles: Dictionary = {}
